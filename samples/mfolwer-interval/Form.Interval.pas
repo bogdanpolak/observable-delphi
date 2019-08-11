91b6821f3ca3a7df4abb76f2a60cbb49313cc51d
Bogdan Polak <bogdan.polak@bsc.com.pl>
2019-08-11 20:42:02 +0200

    Done IntervalApp

--- samples/mfolwer-interval/Form.Interval.pas
+++ samples/mfolwer-interval/Form.Interval.pas
@@ -3,8 +3,9 @@ unit Form.Interval;
 interface
 
 uses
-  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+  Winapi.Windows, Winapi.Messages,
+  System.SysUtils, System.Variants, System.Classes,
+  Vcl.Graphics, Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
 
 type
   TForm1 = class(TForm)
@@ -21,7 +22,7 @@ type
     procedure edtLengthFieldExit(Sender: TObject);
   private
     procedure calculateEnd;
-    procedure calculateLenght;
+    procedure calculateLength;
   public
   end;
 
@@ -32,64 +33,61 @@ implementation
 
 {$R *.dfm}
 
-procedure TForm1.calculateEnd;
+function Integer_parseInt(const s: String): Integer;
 begin
-  // TODO:
-  (* Java:
-  try {
-    int start = Integer.parseInt(_startField.getText());
-    int length = Integer.parseInt(_lengthField.getText());
-    int end = start + length;
-    _endField.setText(String.valueOf(end));
-  } catch (NumberFormatException e) {
-    throw new RuntimeException ("Unexpected Number Format Error");
-  }
-  *)
+  Result := StrToInt(s);
 end;
 
-procedure TForm1.calculateLenght;
+function isNotInteger(const s: String): boolean;
+var
+  i: Integer;
 begin
-  // TODO:
-  (* Java:
-  try {
-    int start = Integer.parseInt(_startField.getText());
-    int end = Integer.parseInt(_endField.getText());
-    int length = end - start;
-    _lengthField.setText(String.valueOf(length));
-  } catch (NumberFormatException e) {
-    throw new RuntimeException ("Unexpected Number Format Error");
-  }
-  *)
+  Result := not TryStrToInt(s, i);
+end;
+
+procedure TForm1.calculateEnd;
+var
+  _start: Integer;
+  _length: Integer;
+  _end: Integer;
+begin
+  _start := Integer_parseInt(edtStartField.Text);
+  _length := Integer_parseInt(edtLengthField.Text);
+  _end := _start + _length;
+  edtEndField.Text := _end.ToString;
+end;
+
+procedure TForm1.calculateLength;
+var
+  _start: Integer;
+  _end: Integer;
+  _length: Integer;
+begin
+  _start := Integer_parseInt(edtStartField.Text);
+  _end := Integer_parseInt(edtEndField.Text);
+  _length := _end - _start;
+  edtLengthField.Text := _length.ToString;
 end;
 
 procedure TForm1.edtStartFieldExit(Sender: TObject);
 begin
-  // TODO:
-  (* Java:
-   * if (isNotInteger(_startField.getText()))
-   *   _startField.setText("0");
-   * calculateLength();
-   *)
+  if isNotInteger(edtStartField.Text) then
+    edtStartField.Text := '0';
+  calculateLength();
 end;
 
 procedure TForm1.edtEndFieldExit(Sender: TObject);
 begin
-  // TODO:
-  (* Java:
-   * if (isNotInteger(_endField.getText()))
-   *   _endField.setText("0");
-   * calculateLength();
-   *)
+  if isNotInteger(edtEndField.Text) then
+    edtEndField.Text := '0';
+  calculateLength();
 end;
 
 procedure TForm1.edtLengthFieldExit(Sender: TObject);
 begin
-  // TODO:
-  (* Java:
-   * if (isNotInteger(_lengthField.getText()))
-   *   _lengthField.setText("0");
-   * calculateEnd();
-   *)
+  if isNotInteger(edtLengthField.Text) then
+    edtLengthField.Text := '0';
+  calculateEnd();
 end;
 
 end.
